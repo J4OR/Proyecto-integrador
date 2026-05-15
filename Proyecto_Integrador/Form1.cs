b7d585979ad086140f0d71dcf99f645df3d60e58
J4OR <jortegonrangel@gmail.com>
2026-05-15 16:41:57 -0500

    J4OR

--- Proyecto_Integrador/Form1.cs
+++ Proyecto_Integrador/Form1.cs
@@ -1,3 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
 namespace Proyecto_Integrador
 {
     public partial class Form1 : Form

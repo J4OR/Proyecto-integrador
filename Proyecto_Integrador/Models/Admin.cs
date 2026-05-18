4f5f181c51f04e0f065d3c6be043c4fa2d8d7315
J4OR <jortegonrangel@gmail.com>
2026-05-18 08:46:09 -0500

    J4OR

--- Proyecto_Integrador/Models/Admin.cs
+++ Proyecto_Integrador/Models/Admin.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Proyecto_Integrador.Models
 {
-    internal class Admin
+    public class Admin : Persona
     {
+        public string password { get; set; }
+        public string User { get; set; }
+
+        [JsonConstructor]
+        public Admin(int id, string nit, string nombre, string password, string user, bool state) : base(id, nit, nombre)
+        {
+            this.password = password;
+            this.state = state;
+            this.User = user;
+        }
     }
 }

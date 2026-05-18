66bfead061779a36f767558fa46739ed334ac3a4
J4OR <jortegonrangel@gmail.com>
2026-05-18 08:46:09 -0500

    J4OR

--- Proyecto_Integrador/Models/Persona.cs
+++ Proyecto_Integrador/Models/Persona.cs
@@ -5,16 +5,16 @@ using System.Text.Json.Serialization;
 
 namespace Proyecto_Integrador.Models
 {
-    internal class Persona
+    public class Persona
     {
         public int Id { get; set; }
         public string nit { get; set; }
         public string nombre { get; set; }
 
         [JsonConstructor]
-        public Persona(int id, string nit, string nombre)
+        public Persona (string nit, string nombre)
         {
-            this.Id = id;
+            this.Id += 1;
             this.nit = nit;
             this.nombre = nombre;
         }
--- Proyecto_Integrador/Models/Usuario.cs
+++ Proyecto_Integrador/Models/Usuario.cs
@@ -5,16 +5,17 @@ using System.Text.Json.Serialization;
 
 namespace Proyecto_Integrador.Models
 {
-    internal class Usuario : Persona
+    public class Usuario : Persona
     {
-        public string clave { get; set; }
+        public string password { get; set; }
         public string User { get; set; }
         public bool state { get; set; }
+        int idPersona { get; set; }
 
         [JsonConstructor]
-        public Usuario(string clave, string user, bool state) : base(id, nit, nombre)
+        public Usuario(int id, string nit, string nombre, string password, string user, bool state) : base(id, nit, nombre)
         {
-            this.clave = clave;
+            this.password = password;
             this.state = state;
             this.User = user;
         }

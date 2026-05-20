dd17704996dd26ef35f4d2089aa49531d7c538cb
J4OR <jortegonrangel@gmail.com>
2026-05-20 14:53:54 -0500

    J4OR

---
+++ Proyecto_Integrador/Repository/ClienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using Proyecto_Integrador.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto_Integrador.Repository
+{
+    public class ClienteRepository
+    {
+        
+    }
+}

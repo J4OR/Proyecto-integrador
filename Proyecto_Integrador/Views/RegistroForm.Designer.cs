bdb956fbf4093c8552a8b4776ad71d20edd027c5
DEIDER DIAZ <deideralfonsodiaz@unicesar.edu.co>
2026-05-20 11:37:34 -0500

    DEIDER

--- Proyecto_Integrador/Views/RegistroForm.Designer.cs
+++ Proyecto_Integrador/Views/RegistroForm.Designer.cs
@@ -28,18 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtUser = new TextBox();
+            lblTitullo = new Label();
             SuspendLayout();
+            // 
+            // txtUser
+            // 
+            txtUser.AccessibleDescription = "";
+            txtUser.Font = new Font("Segoe UI", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            txtUser.ForeColor = SystemColors.InactiveCaption;
+            txtUser.Location = new Point(540, 195);
+            txtUser.Multiline = true;
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(366, 45);
+            txtUser.TabIndex = 7;
+            txtUser.Text = "Ingrese su usuario";
+            // 
+            // lblTitullo
+            // 
+            lblTitullo.AutoSize = true;
+            lblTitullo.Font = new Font("Segoe UI", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTitullo.Location = new Point(620, 53);
+            lblTitullo.Name = "lblTitullo";
+            lblTitullo.Size = new Size(167, 50);
+            lblTitullo.TabIndex = 13;
+            lblTitullo.Text = "Registro";
             // 
             // RegistroForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(972, 627);
+            Controls.Add(lblTitullo);
+            Controls.Add(txtUser);
             Name = "RegistroForm";
             Text = "RegistroForm";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private TextBox txtUser;
+        private Label lblTitullo;
     }
 }

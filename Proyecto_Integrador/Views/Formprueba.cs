66ef0e46da9e3c5b6a469c45e759fc517499097c
J4OR <jortegonrangel@gmail.com>
2026-05-20 18:02:06 -0500

    J4OR

---
+++ Proyecto_Integrador/Views/Formprueba.cs
@@ -0,0 +1,25 @@
+﻿using Proyecto_Integrador.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Proyecto_Integrador.Views
+{
+    public partial class Form_prueba : Form
+    {
+        ClienteController clienteController = new ClienteController();
+        public Form_prueba()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

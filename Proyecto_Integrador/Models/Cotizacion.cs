57dccc7873fb07fc599f2b83454e63ea5fee9348
J4OR <jortegonrangel@gmail.com>
2026-06-01 13:37:50 -0500

    J4OR

--- Proyecto_Integrador/Models/Cotizacion.cs
+++ Proyecto_Integrador/Models/Cotizacion.cs
@@ -8,17 +8,14 @@ namespace Proyecto_Integrador.Models
     public class Cotizacion
     {
         public string id { get; set; }
-        public int numero { get; set; }
         public Cliente cliente { get; set; }
         public Terreno terreno { get; set; }
         public Material material { get; set; }
         public double costoTotal { get; set; }
         public DateTime fecha { get; set; }
-        public string estado { get; set; }
-        public string observaciones { get; set; }
-
+        public bool estado { get; set; }
         [JsonConstructor]
-        public Cotizacion(string id,Cliente cliente, Terreno terreno,Material material, DateTime fecha, string estado, string observaciones)
+        public Cotizacion(string id,Cliente cliente, Terreno terreno,Material material, DateTime fecha)
         {
             this.id = id;
             this.cliente = cliente;
@@ -26,8 +23,7 @@ namespace Proyecto_Integrador.Models
             this.material = material;
             this.costoTotal = material.precioUnidad * terreno.volumen;
             this.fecha = fecha;
-            this.estado = estado;
-            this.observaciones = observaciones;
+            this.estado = true;
         }
     }
 }
--- Proyecto_Integrador/Views/FormInicio.Designer.cs
+++ Proyecto_Integrador/Views/FormInicio.Designer.cs
@@ -1,45 +1,45 @@
-﻿namespace Proyecto_Integrador.Views
-{
-    partial class FormInicio
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+﻿//namespace Proyecto_Integrador.Views
+//{
+//    partial class FormInicio
+//    {
+//        /// <summary>
+//        /// Required designer variable.
+//        /// </summary>
+//        private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+//        /// <summary>
+//        /// Clean up any resources being used.
+//        /// </summary>
+//        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+//        protected override void Dispose(bool disposing)
+//        {
+//            if (disposing && (components != null))
+//            {
+//                components.Dispose();
+//            }
+//            base.Dispose(disposing);
+//        }
 
-        #region Windows Form Designer generated code
+//        #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            SuspendLayout();
-            // 
-            // FormInicio
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(989, 603);
-            Name = "FormInicio";
-            Text = "FormInicio";
-            ResumeLayout(false);
-        }
+//        /// <summary>
+//        /// Required method for Designer support - do not modify
+//        /// the contents of this method with the code editor.
+//        /// </summary>
+//        private void InitializeComponent()
+//        {
+//            SuspendLayout();
+//            // 
+//            // FormInicio
+//            // 
+//            AutoScaleDimensions = new SizeF(8F, 20F);
+//            AutoScaleMode = AutoScaleMode.Font;
+//            ClientSize = new Size(989, 603);
+//            Name = "FormInicio";
+//            Text = "FormInicio";
+//            ResumeLayout(false);
+//        }
 
-        #endregion
-    }
-}
+//        #endregion
+//    }
+//}

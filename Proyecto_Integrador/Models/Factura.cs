fa767f79125eb03b3e0a7c59a666ab12394fee48
J4OR <jortegonrangel@gmail.com>
2026-06-09 18:45:48 -0500

    J4OR

--- Proyecto_Integrador/Models/Factura.cs
+++ Proyecto_Integrador/Models/Factura.cs
@@ -8,9 +8,12 @@ namespace Proyecto_Integrador.Models
     public class Factura
     {
         public string id { get; set; }
-        public Cotizacion cotizacion { get; set; } 
+        public string observacion { get; set; }
         public DateTime fecha { get; set; }
+        public Cotizacion cotizacion { get; set; } 
+        public List<ItemFactura> items { get; set; }
         public bool estado { get; set; }
+        public decimal subtotal => items.Sum(i => i.valorTotal);
 
         [JsonConstructor]
         public Factura(string id, Cotizacion cotizacion, DateTime fechaEmision)
@@ -19,6 +22,14 @@ namespace Proyecto_Integrador.Models
             this.cotizacion = cotizacion;
             this.fecha = fecha;
             this.estado = true;
+        }
+        public class ItemFactura
+        {
+            public string descripcion { get; set; }
+            public string unidad { get; set; }
+            public decimal cantidad { get; set; }
+            public decimal valorUnitario { get; set; }
+            public decimal valorTotal => cantidad * valorUnitario;
         }
     }
 }
